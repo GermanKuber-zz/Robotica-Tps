8a70feaacfbe4e32955c78e1291d0d68a2430616
Germán Küber <german.kuber@outlook.com>
2017-09-16 12:25:49 -0300

    Tp 3 - parte 1

---
+++ Tp 3/tp3.ino
@@ -0,0 +1,22 @@
+int led = 13;
+int pushButtom = 8;
+int val = 0;
+void setup()
+{
+    pinMode(led, OUTPUT);
+    pinMode(pushButtom, INPUT);
+}
+
+void loop()
+{
+    val = digitalRead(pushButtom); // read input value
+    if (val == HIGH)
+    {                              
+        digitalWrite(led, HIGH); // turn LED OFF
+    }
+    else
+    {
+        digitalWrite(led, LOW); // turn LED ON
+    }
+   
+}
